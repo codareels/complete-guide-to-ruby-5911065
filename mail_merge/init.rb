49862072d45f6a8e702cf735ab511241b14abbec
codareels <codareels@gmail.com>
2025-04-22 13:11:13 +0000

    dhdhd

--- mail_merge/init.rb
+++ mail_merge/init.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 
+require 'erb'
+require 'csv'
+
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
+template_path = File.join(App_ROOT, 'overdue_notice.txt.erb')
+csv_path = File.join(App_ROOT, 'overdue_notice.txt.erb')
+output_dir = File.join(App_ROOT, 'overdue_notice.txt.erb')
 
 def number_to_currency(value)
   sprintf('$%.2f', value.to_f)
